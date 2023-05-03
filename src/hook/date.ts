4d7de41aa13f2c581b4fbdb0ad0cd575855d280c
Владислав Дятлов <datlovvladislav6@gmail.com>
2023-05-03 22:33:05 +0300

    first commit

--- src/hook/date.ts
+++ src/hook/date.ts
@@ -1,26 +1,18 @@
 export const onTime = (old_date: number): string =>{
-  const getDayName = [
-    'Вс',
-    'Пн',
-    'Вт',
-    'Ср',
-    'Чт',
-    'Пт',
-    'Сб',
-  ];
+  const getDayName = ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"];
 
-    const date = new Date();
-    const now = new Date(old_date * 1000);
+  const date = new Date();
+  const now = new Date(old_date * 1000);
 
-    if(now.getDate() === date.getDate()){
-      return 'Сегодня';
-    } else if(now.getDate() === date.getDate() + 1){
-      return 'Завтра';
-    } else{
-      return `${getDayName[now.getDay()]} ${now.getUTCDate()}`;
-    }
-    // данная функция служит для формирования из числа даты полученной из api в такой ответ Вс 30
-    // так же присутствует проверка в которой текущая дата и следующая за ней меняется на Сегодня и Завтра
+  if (now.getDate() === date.getDate()) {
+    return "Сегодня";
+  } else if (now.getDate() === date.getDate() + 1) {
+    return "Завтра";
+  }
+
+  return `${getDayName[now.getDay()]} ${now.getUTCDate()}`;
+  // данная функция служит для формирования из числа даты полученной из api в такой ответ Вс 30
+  // так же присутствует проверка в которой текущая дата и следующая за ней меняется на Сегодня и Завтра
 }
 
 export const onTimes = (old_date: number): string =>{
